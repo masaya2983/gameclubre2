b3a785a2053719ebb67bb741aead5bbfc47d0fc5
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-15 08:36:25 +0000

    remake2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_081641) do
+ActiveRecord::Schema.define(version: 2023_06_15_082833) do
+
+  create_table "coments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "game_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "game_id"
